d188fbc0bfd7d8245f3bfb7b3ed85e715089f74a
Theachiever05 <118321616+Theachiever05@users.noreply.github.com>
2025-07-08 21:49:11 +0530

    Update musicLibrary.py

--- musicLibrary.py
+++ musicLibrary.py
@@ -2,5 +2,6 @@ music = {
     "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
     "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
     "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
-    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21"
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
+    "blindinglights": "https://www.youtube.com/watch?v=4NRXx6U8ABQ&list=RD4NRXx6U8ABQ&start_radio=1"
 }
